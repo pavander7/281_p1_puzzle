c0ff4380c57d44f3c2c86f9ddc47f1a3125b9491
pavander <pavander@umich.edu>
2023-09-11 14:58:17 -0400

    getoptlong working

--- puzzle.cpp
+++ puzzle.cpp
@@ -10,21 +10,21 @@ int main(int argc, char* argv[]) {
     
     cout << "flag 1" << endl;
     
-    /*static struct option long_options[] = {
+    static struct option long_options[] = {
     {"help",    no_argument,        NULL,  'h'},
     {"queue",   no_argument,        NULL,  'q'},
     {"stack",   no_argument,        NULL,  's'},
-    {"output",  required_argument,  NULL,  'o'},
+    {"output",  required_argument,  0,  'o'},
     {0,         0,                  NULL,  0, }
-    };*/
+    };
 
-    //getopt_long(argc, argv, "hqso:", long_options, &option_index)
+    
 
-    //int option_index = 0;
+    int option_index = 0;
     int dataType = 0;
     int outputType = 0;
     cout << "flag 1" << endl;
-    int c = getopt(argc, argv, "hqso:");
+    int c = getopt_long(argc, argv, "hqso:", long_options, &option_index);
     while (c != -1) {
         cout << "flag 2 " << c << endl;
         if (c == 'h') {
@@ -35,13 +35,16 @@ int main(int argc, char* argv[]) {
             dataType = 1;
         } else if (c == 'o') {
             string temp = optarg;
+            cout << "slay 1" << endl;
             if (temp == "map") {
                 outputType = 0;
+                cout << "slay 2.1: " << optind << endl;
             } else if (temp == "list") {
                 outputType = 1;
+                cout << "slay 2.2: " << optind << endl;
             } else return 1;
         } else return 1;
-        c = getopt(argc, argv, "hqso:");
+        c = getopt_long(argc, argv, "hqso:", long_options, &option_index);
     }
     
 }
