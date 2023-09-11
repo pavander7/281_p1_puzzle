c3ed502e18bbb70f5d8f7bed21fabbfe6855f0c6
pavander <pavander@umich.edu>
2023-09-11 14:59:21 -0400

    getoptlong working but without the flags

--- puzzle.cpp
+++ puzzle.cpp
@@ -23,25 +23,25 @@ int main(int argc, char* argv[]) {
     int option_index = 0;
     int dataType = 0;
     int outputType = 0;
-    cout << "flag 1" << endl;
+    //cout << "flag 1" << endl;
     int c = getopt_long(argc, argv, "hqso:", long_options, &option_index);
     while (c != -1) {
-        cout << "flag 2 " << c << endl;
+        //cout << "flag 2 " << c << endl;
         if (c == 'h') {
-            cout << "help menu" << endl; //UPDATE
+            //cout << "help menu" << endl; //UPDATE
         } else if (c == 'q') {
             dataType = 0;
         } else if (c == 's') {
             dataType = 1;
         } else if (c == 'o') {
             string temp = optarg;
-            cout << "slay 1" << endl;
+            //cout << "slay 1" << endl;
             if (temp == "map") {
                 outputType = 0;
-                cout << "slay 2.1: " << optind << endl;
+                //cout << "slay 2.1: " << optind << endl;
             } else if (temp == "list") {
                 outputType = 1;
-                cout << "slay 2.2: " << optind << endl;
+                //cout << "slay 2.2: " << optind << endl;
             } else return 1;
         } else return 1;
         c = getopt_long(argc, argv, "hqso:", long_options, &option_index);
