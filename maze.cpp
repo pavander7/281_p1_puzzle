9d5caa464584964d282dd5b43016fc70dbc424ed
pavander <pavander@umich.edu>
2023-09-17 17:25:19 -0400

    boundary checking added

--- maze.cpp
+++ maze.cpp
@@ -193,10 +193,14 @@ void player::investigate(bool button, vector<vector<vector<bool> > > &discoverMa
         discover({current_state.color, current_state.row, current_state.col}, discoverMap, y);
     } else {
         cout << "investigating surroundings" << endl;
-        discover({current_state.color, current_state.row - 1, current_state.col}, discoverMap, y); //north
-        discover({current_state.color, current_state.row, current_state.col + 1}, discoverMap, y); //east
-        discover({current_state.color, current_state.row + 1, current_state.col}, discoverMap, y); //south
-        discover({current_state.color, current_state.row, current_state.col - 1}, discoverMap, y); //west
+        if (current_state.row > 0)
+            discover({current_state.color, current_state.row - 1, current_state.col}, discoverMap, y); //north
+        if (current_state.col > 0)
+            discover({current_state.color, current_state.row, current_state.col + 1}, discoverMap, y); //east
+        if (current_state.row < y.height)
+            discover({current_state.color, current_state.row + 1, current_state.col}, discoverMap, y); //south
+        if (current_state.col < y.width)
+            discover({current_state.color, current_state.row, current_state.col - 1}, discoverMap, y); //west
     }
     search_container.pop_front();
     current_state = search_container.front();
