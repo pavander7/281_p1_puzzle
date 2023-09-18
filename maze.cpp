a1dda3a4bc8ecf55c3da1190a4b95c38ddcc42e5
pavander <pavander@umich.edu>
2023-09-18 12:19:53 -0400

    fuck u caen

--- maze.cpp
+++ maze.cpp
@@ -177,7 +177,9 @@ void player::discover(state x, vector<vector<vector<bool> > > &discoverMap, Maze
 }
 
 bool Maze::checkDiscover(state x){
-    return discoverMap[x.row][x.col][size_t(x.color - 97)];
+    if (x.row >= width || x.col >= height || x.color - 0 >= int(num_colors + 97) || x.row < 0 || x.col < 0) {
+        return true;
+    } else return discoverMap[x.row][x.col][size_t(x.color - 97)];
 }
 
 bool player::checkButton(state x, Maze &y){
